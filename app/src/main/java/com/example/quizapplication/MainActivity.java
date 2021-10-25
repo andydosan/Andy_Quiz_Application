a2e4301894dec4b7fc280c7ac7f0fbf87fb4e4d1
adosan <adosan@students.logoscambodia.org>
2021-10-25 12:09:09 +0700

    Removed int bruh

--- app/src/main/java/com/example/quizapplication/MainActivity.java
+++ app/src/main/java/com/example/quizapplication/MainActivity.java
@@ -33,7 +33,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     TextView text;
     private Spinner spinnerQuizzes;
     public static String quizname;
-    int bruh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
